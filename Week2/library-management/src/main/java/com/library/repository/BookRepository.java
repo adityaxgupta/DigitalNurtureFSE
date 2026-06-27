6f0814110d251d063db0a14f05ceb36dc821a2aa
Aditya <146020942+adityaxgupta@users.noreply.github.com>
2026-06-27 20:16:34 +0530

    Week 2 Spring Core exercise 1 & 2

---
+++ Week2/library-management/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void performDatabaseOperation() {
+        System.out.println("BookRepository: Accessing the database...");
+    }
+}
